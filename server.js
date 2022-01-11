43cd1232c71903580d2d71d8eb313bf3df3e73ef
Taimur Israr <taimurisrar806@gmail.com>
2022-01-11 16:46:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const app = express();
 
+const nodemailer = require("nodemailer");
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(PORT, ()=>{
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        host: "mail.maseergroup.com",
+        port: 110,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'receiver@maseergroup.com',
+            pass: 'GMAILReceiver001'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'receiver@maseergroup.com',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT, () => {
     console.log(`server chal raha hai ${PORT}`)
 })
